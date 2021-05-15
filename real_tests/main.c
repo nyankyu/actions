b68b9ad793cb558cb20505a83fac5267b7c77804
nohtou <nohtou@student.42tokyo.jp>
2021-05-15 09:51:39 +0900

    del leaks

--- real_tests/main.c
+++ real_tests/main.c
@@ -1,6 +1,6 @@
 int	ft_strlen_launcher(void);
 int	ft_atoi_launcher(void);
-#include <stdlib.h>
+
 int	main(void)
 {
 	int	result;
@@ -8,9 +8,8 @@ int	main(void)
 	result = 0;
 	result += ft_strlen_launcher();
 	result += ft_atoi_launcher();
-	system("leaks $$");
 	if (result != 0)
 		return (1);
 	else
-		return (0);
+		return (1);
 }
